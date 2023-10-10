744a43808db37fc51c597c9f868085e3d4a91105
CharlieLive <karol.kozaczuk@gmail.com>
2023-10-10 20:16:48 +0200

    update

--- class.js
+++ class.js
@@ -1,12 +1,44 @@
 class Pokemon {
-    constructor(name, HP, element) {
-        this.name = name;
-        this.HP = HP;
-        this.element = element;
-    }
+  #name;
+  #HP;
+  constructor({ name, HP, element }) {
+    this.#name = name;
+    this.#HP = HP;
+    this.element = element;
+  }
+
+  get HP() {
+    return this.#HP;
+  }
+  set HP(newHP) {
+    this.#HP = newHP;
+  }
 }
 
-const pikachu = new Pokemon("Pikachu", 73, "electric");
+const pikachu = new Pokemon({ name: "Pikachu", HP: 73, element: "Electric" });
 
 table(pikachu);
 
+// // Object create - inheritance
+// const bulbasaur = Object.create(pikachu);
+
+// log(bulbasaur.name);
+
+// log(bulbasaur.hasOwnProperty("name"));
+
+const charmander = new Pokemon({ name: "Charmander", HP: 20 , element: "Grass" });
+
+table(charmander)
+
+
+
+// // charmander.name = "bulbasaur";
+
+// // log(charmander.name)
+
+// // log(pikachu.isPrototypeOf(bulbasaur));
+
+// log("Prototype:", this);
+// log("Prototype:I", this.__proto__);
+// log("Prototype:II", this.__proto__.__proto__);
+// log("Prototype:III", this.__proto__.__proto__.__proto__);
---
+++ ulility.js
@@ -0,0 +1,3 @@
+const { log } = conosle;
+const { table } = console;
+
